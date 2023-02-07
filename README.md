537c0218c2488ccc5a1239027e333b169ca1ab2d
chudilo4 <chipalinchik@gmail.com>
2023-02-07 12:26:04 +0500

    add lint and test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Chudilo4/python-project-52/workflows/hexlet-check/badge.svg)](https://github.com/Chudilo4/python-project-52/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/4468e7a3dfec689d9dd5/maintainability)](https://codeclimate.com/github/Chudilo4/python-project-52/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/4468e7a3dfec689d9dd5/test_coverage)](https://codeclimate.com/github/Chudilo4/python-project-52/test_coverage)
 ### DOMAIN NAME: https://webserver.up.railway.app/
--- task_manager/tests/test.py
+++ task_manager/tests/test.py
@@ -50,25 +50,95 @@ class CRUD_Label_Test(TestCase):
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='test', password='123456789Zz')
-        self.label = Label.objects.create(name='123')
+        self.label = Label.objects.create(name='789')
         self.client.login(username='test', password='123456789Zz')
 
-    def created_label(self):
+    def test_created_label(self):
         response = self.client.post(reverse('label_create'),
                                     {'name': '123'},
                                     follow=True)
         self.assertEqual(Label.objects.get(name='123').name, '123')
         self.assertRedirects(response, reverse_lazy('labels'))
 
-    def update_label(self):
-        response = self.client.post(reverse('label_create', kwargs={'pk': self.label.pk}),
+    def test_update_label(self):
+        response = self.client.post(reverse('label_update', kwargs={'pk': self.label.pk}),
                                     {'name': '456'},
                                     follow=True)
         self.assertEqual(Label.objects.get(name='456').name, '456')
-        self.assertRedirects(response, reverse_lazy('labels'), target_status_code=302)
+        self.assertRedirects(response, reverse_lazy('labels'))
 
-    def delete_label(self):
+    def test_delete_label(self):
         response = self.client.post(reverse('label_delete', kwargs={'pk': self.label.pk}),
                                     follow=True)
-        self.assertEqual(Label.objects.filter(name='123').count(), 0)
-        self.assertRedirects(response, reverse_lazy('labels'), target_status_code=302)
+        self.assertEqual(Label.objects.filter(name='789').count(), 0)
+        self.assertRedirects(response, reverse_lazy('labels'))
+
+
+class CRUD_Status_Test(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='test', password='123456789Zz')
+        self.status = Status.objects.create(title='123')
+        self.client.login(username='test', password='123456789Zz')
+
+    def test_create_status(self):
+        response = self.client.post(reverse_lazy('status_create'),
+                                    {'title': '456'
+                                     }, follow=True)
+        self.assertEqual(Status.objects.get(title='456').title, '456')
+        self.assertRedirects(response, reverse_lazy('status'))
+
+    def test_update_status(self):
+        response = self.client.post(reverse('status_update', kwargs={'pk': self.status.pk}),
+                                    {'title': '456'}, follow=True)
+        self.assertEqual(Status.objects.get(pk=self.status.pk).title, '456')
+        self.assertRedirects(response, reverse_lazy('status'))
+
+    def test_delete_status(self):
+        response = self.client.post(reverse('status_delete', kwargs={'pk': self.status.pk}), follow=True)
+        self.assertRedirects(response, reverse_lazy('status'))
+        self.assertEqual(Status.objects.filter(title='123').count(), 0)
+
+
+class CRUD_Task_Test(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='test', password='123456789Zz')
+        self.executor = User.objects.create_user(username='test2', password='123456789Zz')
+        self.status = Status.objects.create(title='123')
+        self.label = Label.objects.create(name='789')
+        self.label2 = Label.objects.create(name='777')
+        self.new_task = self.label.task_set.create(title='qwe',
+                                                   description='qwerty',
+                                                   status=self.status,
+                                                   executor=self.executor,
+                                                   author=self.user)
+        self.client.login(username='test', password='123456789Zz')
+
+    def test_created_task(self):
+        response = self.client.post(reverse_lazy('task_create'),
+                                    {'title': 'Тестовая задача',
+                                     'description': 'Описание тестовой задачи',
+                                     'status': self.status.pk,
+                                     'executor': self.executor.pk,
+                                     'labels': self.label.pk
+                                     }, follow=True)
+        self.assertEqual(Task.objects.get(description='Описание тестовой задачи').title, 'Тестовая задача')
+        self.assertRedirects(response, reverse_lazy('tasks'))
+
+    def test_update_task(self):
+        response = self.client.post(reverse('task_update', kwargs={'pk': self.new_task.pk}),
+                                    {'title': 'Тестовая задача2',
+                                     'description': 'Описание тестовой задачи2',
+                                     'status': self.status.pk,
+                                     'executor': self.executor.pk,
+                                     'labels': self.label2.pk
+                                     }, follow=True)
+        self.assertEqual(Task.objects.get(pk=self.new_task.pk).title, 'Тестовая задача2')
+        self.assertRedirects(response, reverse_lazy('tasks'))
+
+    def test_task_delete(self):
+        response = self.client.post(reverse('task_delete', kwargs={'pk': self.new_task.pk}),
+                                    follow=True)
+        self.assertEqual(Task.objects.filter(pk=self.new_task.pk).count(), 0)
+        self.assertRedirects(response, reverse_lazy('tasks'))

d474bf57527d0c33fdc2bca3c37f6af49551e79b
chudilo4 <chipalinchik@gmail.com>
2023-05-20 19:34:17 +0500

    тра та та

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Status(models.Model):
@@ -16,12 +17,12 @@ class Status(models.Model):
 class Task(models.Model):
     name = models.CharField(blank=False, null=False, verbose_name='Имя', max_length=15)
     description = models.TextField(verbose_name='Описание')
-    executor = models.ForeignKey(CustomUser, on_delete=models.PROTECT,
+    executor = models.ForeignKey(User, on_delete=models.PROTECT,
                                  verbose_name='Исполнитель', related_name='task_exectot')
     status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус',
                                related_name='task_status')
     label = models.ManyToManyField("Label", through='LabelsOfTask', verbose_name='Метки')
-    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT, verbose_name='Автор',
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Автор',
                                related_name='task_author')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     update_time = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')

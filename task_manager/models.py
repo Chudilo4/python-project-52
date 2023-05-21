28f023b56d587a9e2681c0b4c7cfea05b7649ad0
chudilo4 <chipalinchik@gmail.com>
2023-05-21 21:34:11 +0500

    , blank=True, null=True

--- task_manager/models.py
+++ task_manager/models.py
@@ -18,10 +18,12 @@ class Task(models.Model):
     name = models.CharField(blank=False, null=False, verbose_name='Имя', max_length=255)
     description = models.TextField(verbose_name='Описание')
     executor = models.ForeignKey(CustomUser, on_delete=models.PROTECT,
-                                 verbose_name='Исполнитель', related_name='task_exectot')
+                                 verbose_name='Исполнитель', related_name='task_exectot',
+                                 blank=True, null=True)
     status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name='Статус',
                                related_name='task_status')
-    label = models.ManyToManyField("Label", through='LabelsOfTask', verbose_name='Метки')
+    label = models.ManyToManyField("Label", through='LabelsOfTask', verbose_name='Метки',
+                                   blank=True, null=True)
     author = models.ForeignKey(CustomUser, on_delete=models.PROTECT, verbose_name='Автор',
                                related_name='task_author')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')

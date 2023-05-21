637157354cacc8158aa03cf694e5609e103f4d77
chudilo4 <chipalinchik@gmail.com>
2023-05-21 10:50:21 +0500

    тра та та

--- task_manager/models.py
+++ task_manager/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Status(models.Model):
     name = models.CharField(blank=False, null=False,
-                            verbose_name='Имя', max_length=15)
+                            verbose_name='Имя', max_length=255)
     created_time = models.DateTimeField(auto_now_add=True,
                                         verbose_name='Дата создания')
     update_time = models.DateTimeField(auto_now=True,
